3a3bb6e57e8c82573f9cad8587c292e3d7d40989
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-21 20:56:53 -0300

    update

---
+++ logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/desafio/desafio-03.js
@@ -0,0 +1,29 @@
+let cont = 1;
+
+while (cont <= 10) {
+    alert(`contador: ${cont}`)
+    cont++
+}
+
+let cont = 10;
+
+while (cont >= 0) {
+    alert(`contador: ${cont}`)
+    cont--
+}
+
+let cont = parseInt(prompt("Qual número você quer começar a contagem regressiva?"));
+
+while (cont >= 0) {
+    console.log(`contador: ${cont}`)
+    cont--
+}
+
+
+let cont = 0;
+let finalNumber = parseInt(prompt("Até qual número você quer contar?"));
+
+while (cont <= finalNumber) {
+    console.log(`contador: ${cont}`)
+    cont++
+}
