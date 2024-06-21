b6017b2284568e6b1724d4d9569c7faa57ff1f71
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-21 18:27:30 -0300

    update

--- logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/app.js
+++ logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/app.js
@@ -2,6 +2,9 @@ alert("Boas vindas ao jogo do número secreto");
 let secretNumber = 29;
 let userGuess = prompt("Escolha um número entre 1 e 30: ");
 
+//se o chute for igual ao número secreto
 if (userGuess == secretNumber) {
-    console.log("Isso aí! Você descobriu o número secreto!");
+    alert(`Isso aí! Você descobriu o número secreto (${secretNumber})!`);
+} else {
+    alert("Você errou :(")              ;
 }
--- logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/desafio/desafio-01.js
+++ logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/desafio/desafio-01.js
@@ -13,6 +13,7 @@ alert(mensagemDeErro);
 
  nome = prompt("Qual é o seu nome?");
  idade = prompt("Qual é a sua idade?");
+ idade = Number(idade);
 
 if (idade >= 18) {
     alert("Pode tirar a habilitação!")
