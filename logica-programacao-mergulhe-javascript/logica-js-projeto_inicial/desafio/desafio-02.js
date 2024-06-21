5381117eb93ffab0ffd977abc11a5c572ed0b77a
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-21 19:11:46 -0300

    Update desafio-02.js

--- logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/desafio/desafio-02.js
+++ logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/desafio/desafio-02.js
@@ -5,3 +5,11 @@ if (dayOfWeek == "Sábado" || dayOfWeek == "Domingo"){
 } else {
     alert("Boa semana!");
 }
+
+let userNumber = parseInt(prompt("Digite um número para verificar se é negativo ou positivo"));
+
+if (userNumber > 0) {
+    alert(`O número ${userNumber} é positivo!`);
+} else {
+    alert(`O número ${userNumber} é negativo!`);
+}
