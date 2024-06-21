de6966dcb105e5f78a21714135fba68a4ec836ad
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-21 20:04:46 -0300

    Update app.js

--- logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/app.js
+++ logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/app.js
@@ -1,18 +1,21 @@
 alert("Boas vindas ao jogo do número secreto");
 let secretNumber = 29;
 let userGuess = null;
+let attempt = 1;
 
 //loop enquanto o chute não for igual ao número secreto
 while (userGuess != secretNumber){
     userGuess = prompt("Escolha um número entre 1 e 30: ");
     //se o chute for igual ao número secreto
     if (userGuess == secretNumber) {
-        alert(`Isso aí! Você descobriu o número secreto (${secretNumber})!`);
+        alert(`Isso aí! Você descobriu o número secreto (${secretNumber}) com ${attempt}!`);
     } else {
         if (userGuess > secretNumber) {
             alert(`O número secreto é menor que ${userGuess}`);
         } else {
             alert(`O número secreto é maior que ${userGuess}`);
         }
+        //attempt = attempt +1;
+        attempt++;
     }
 }
