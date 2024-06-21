3aed5a7c87e7d231155eb30b986f7119c1dbe66e
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-21 18:56:25 -0300

    Create desafio-02.js

---
+++ logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/desafio/desafio-02.js
@@ -0,0 +1,7 @@
+let dayOfWeek = prompt("Que dia da semana é hoje?")
+
+if (dayOfWeek == "Sábado" || dayOfWeek == "Domingo"){
+    alert("Bom fim de semana!");
+} else {
+    alert("Boa semana!");
+}
