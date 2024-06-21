4240595c601f558bfdc74de9d9da075b71787318
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-21 17:56:56 -0300

    Update app.js

--- logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/app.js
+++ logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/app.js
@@ -1,1 +1,7 @@
 alert("Boas vindas ao jogo do número secreto");
+let secretNumber = 29;
+let userGuess = prompt("Escolha um número entre 1 e 30: ");
+
+if (userGuess == secretNumber) {
+    console.log("Isso aí! Você descobriu o número secreto!");
+}
