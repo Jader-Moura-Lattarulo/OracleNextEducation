8ca87bf9a2eff849197b426201b0667c9b43f142
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-22 14:01:37 -0300

    Create desafio-04.js

---
+++ logica-programacao-mergulhe-javascript/logica-js-projeto_inicial/desafios/desafio-04.js
@@ -0,0 +1,33 @@
+/* function welcomeMessage(){
+    console.log("Bem-vindo ao nosso programa!");
+}
+
+welcomeMessage(); 
+
+let nome = "Jader";
+console.log(`Bem-vindo ao nosso programa ${nome}!`);
+
+let nome = "Jader";
+alert(`Olá, ${nome}`);
+
+let linguagemProgramacao = prompt("Qual a linguagem de programação que você mais gosta?");
+console.log(`${linguagemProgramacao}`);
+
+let valor1 = 5;
+let valor2 = 7;
+let resultado = valor1 + valor2;
+
+console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`);
+
+let valor1 = 5;
+let valor2 = 7;
+let resultado = valor1 - valor2;
+
+console.log(`A subtração de ${valor1} e ${valor2} é igual a ${resultado}`);
+
+let idade = parseInt(prompt("Qual a sua idade? (apenas números)"));
+
+let maiorMenor = idade >= 18 ? "maior" : "menor";
+let anoAnos = idade == 1 ? "ano" : "anos";
+alert(`A maior idade no Brasil é considerada a partir dos 18 anos, portanto ${idade} ${anoAnos} é ${maiorMenor} de idade.`);*/
+
