02f9f1920214b43e31257f88d5685b2e71dbd869
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-23 16:03:27 -0300

    Update app.js

--- logica-programacao-mergulhe-javascript/logica-programacao-funcoes-listas/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ logica-programacao-mergulhe-javascript/logica-programacao-funcoes-listas/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,11 +1,17 @@
+let secretNumber = generateRandomNumber();
+
 function showTextOnScren (tag, text){
     let field = document.querySelector(tag);
     field.innerHTML = text;
 }
 
-showTextOnScren("h1", "Jogo do Nº Secreto");
+showTextOnScren("h1", "Jogo do Nº Secreto 2.0");
 showTextOnScren("p", "Escolha um nº entre 1 e 10:");
 
 function checkKick(){
     console.log("O botão foi clicado");
+}
+
+function generateRandomNumber() {
+    return parseInt(Math.random() * 10 + 1);
 }
