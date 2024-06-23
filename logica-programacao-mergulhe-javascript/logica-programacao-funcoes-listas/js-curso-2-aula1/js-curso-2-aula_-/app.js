1cf6a6e8baa68cfc017a6e4b9cff75dcfd274d49
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-23 14:43:14 -0300

    Add titulo, paragrafo e chute

---
+++ logica-programacao-mergulhe-javascript/logica-programacao-funcoes-listas/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+let paragrafo = document.querySelector('p');
+
+titulo.innerHTML = "Jogo do Nº Secreto";
+paragrafo.innerHTML = "Escolha um nº entre 1 e 10";
+
+function checkKick(){
+
+}
