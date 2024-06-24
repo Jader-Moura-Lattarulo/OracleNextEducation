3e162e3cf69ee1775dfd926c405b5df12543e593
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-24 12:18:09 -0300

    update

--- logica-programacao-mergulhe-javascript/logica-programacao-funcoes-listas/desafios-02/desafios-03/desafios-03.js
+++ logica-programacao-mergulhe-javascript/logica-programacao-funcoes-listas/desafios-02/desafios-03/desafios-03.js
@@ -17,3 +17,21 @@ function doubleTheNumber(){
     return alert(`O dobro do número ${number} é ${double}.`);
 }
 
+function whichIsBigger(){
+    let firstInput = document.querySelector("#get_first_number");
+    let firstNumber = firstInput.value;
+
+    let secoundInput = document.querySelector("#get_secound_number");
+    let secoundNumber = secoundInput.value;
+
+    let thirdInput = document.querySelector("#get_third_number");
+    let thirdNumber = thirdInput.value;
+
+    if (firstNumber > secoundNumber && firstNumber > thirdNumber) {
+        return alert(`o número ${firstNumber} é o maior número entre os três.`);
+    } else if (secoundNumber > firstNumber && secoundNumber > thirdNumber) {
+        return alert(`o número ${secoundNumber} é o maior número entre os três.`);
+    } else {
+        return alert(`o número ${thirdNumber} é o maior número entre os três.`);
+    }
+}
