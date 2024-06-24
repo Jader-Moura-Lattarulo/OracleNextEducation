bef6c8de94c1c49c778316b616d481523e96fa0f
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-24 17:51:09 -0300

    update

--- logica-programacao-mergulhe-javascript/logica-programacao-funcoes-listas/desafios-04/desafios-04.js
+++ logica-programacao-mergulhe-javascript/logica-programacao-funcoes-listas/desafios-04/desafios-04.js
@@ -4,12 +4,12 @@ de 18,5 até 24,99 - Normal
 de 25 até 29,99 - Sobrepeso
 maior que 30 - Obesidade*/
 
-function showResultOnScreen (tag, text){
-    let field = document.querySelector(tag);
+function showResultOnScreen (id, text){
+    let field = document.getElementById(id);
     field.innerHTML = text;
 }
 function showInitialMsg(){
-    showResultOnScreen("p", "Verifique acima seu IMC.");
+    showResultOnScreen("resultTxt", `Preencha os campos e clique em "Calcular" para verificar seu IMC.`);
 }
 
 showInitialMsg();
@@ -22,12 +22,26 @@ function bodyMassIndex(){
     
     if (bodyMassIndex >= 18.51 && bodyMassIndex <= 24.99) {
         //normal
-        showResultOnScreen("p",`Seu IMC está em ${bodyMassIndex}, e é considerado normal, não tem com o que se preocupar.`);      
+        showResultOnScreen("resultTxt",`Seu IMC está em ${bodyMassIndex}, e é considerado normal, não tem com o que se preocupar.`); 
     } else {
         //Baixo, Sobrepeso, Obesidade
         let wordLowOverweightObesity = bodyMassIndex <= 18.5 ? "baixo" : bodyMassIndex >= 25 && bodyMassIndex <= 29.99 ? "sobrepeso" : "obesidade";
         let msgLowOverweightObesity = `Seu IMC está em ${bodyMassIndex} e é considerado ${wordLowOverweightObesity}, procure um profissional da saúde para saber o melhor tratatemto.`;
-        showResultOnScreen("p", msgLowOverweightObesity);
+        showResultOnScreen("resultTxt", msgLowOverweightObesity);
     }
+    showResultOnScreen("newCalculationTxt", `Clique em "Novo Cálculo" para um novo cálculo de IMC.`);
+    document.getElementById("newCalculation").removeAttribute("disabled");
 }
 
+function cleanInput(){
+    weight = document.querySelector("#get_weight");
+    weight.value ="";
+    height = document.querySelector("#get_height");
+    height.value ="";
+}
+
+function newCalculation(){
+    cleanInput();
+    showInitialMsg();
+    document.getElementById("newCalculation").setAttribute("disabled",true);
+}
