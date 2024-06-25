a998af42f416ea81c56d8aee9cd2f6cedd226e26
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-06-25 16:17:10 -0300

    Update desafio-array.js

---
+++ logica-programacao-mergulhe-javascript/logica-programacao-funcoes-listas/desafios-array/desafio-array.js
@@ -0,0 +1,29 @@
+//Crie uma lista vazia, com o nome listaGenerica
+
+let listaGenerica = [];
+
+/* Crie uma lista de linguagens de programação chamada linguagensDeProgramacao
+com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.*/
+
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+
+/* Adicione à lista linguagensDeProgramacao os seguintes
+elementos: 'Java', 'Ruby' e 'GoLang'. */
+
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.*/
+
+let listaTresNomes = ["Jader", "Gabi", "Laura"];
+
+console.log(listaTresNomes[0]);
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.*/
+
+console.log(listaTresNomes[1]);
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o terceiro elemento.*/
+
+console.log(listaTresNomes[2]);
